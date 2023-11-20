5900b7c4f29341650c1a143f87147577257585a6
Romain VEILLARD <rveillard.sopra@lfdj.com>
2023-11-20 10:01:13 +0100

    🐛 Use HashRouter to make gh-pages work

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import Quiz from './Quiz/Quiz';
 import QuizResults from './QuizResults/QuizResults';
 
 function App() {
   return (
     <Container className="App mt-5">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Quiz />} />
           <Route path="/results" element={<QuizResults />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Container>
   );
 }
